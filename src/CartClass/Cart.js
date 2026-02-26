08b31a7461a7a1f1f445c374927521dd6fd62672
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-02-26 12:25:28 +0100

    create Cart class for cart object (IN JS)

---
+++ src/CartClass/Cart.js
@@ -0,0 +1,39 @@
+
+class Cart {
+    constructor() {
+        this.cartProducts = [];
+    }
+
+    addToCart(item, quantity) {
+        const temp = this.findCartItem(item.id)
+        if (temp) {
+            temp.quantity += quantity;
+            return;
+        }
+
+        const newItem = {
+            item: item,
+            quantity: quantity
+        }
+
+        this.cartProducts.push(newItem);
+    }
+
+    removeFromCart(id) {
+        this.cartProducts = this.cartProducts.filter(cartItem => cartItem.item.id !== id);
+    }
+
+    changeQuantity(id, quantity) {
+        const item = this.findCartItem(id);
+
+        if (quantity !== item.quantity) {
+            item.quantity = quantity;
+        }
+    }
+
+    findCartItem(id) {
+        return this.cartProducts.find(cartItem => cartItem.item.id === id);
+    }
+}
+
+export default Cart
---
+++ src/CartClass/Cart.test.js
@@ -0,0 +1,43 @@
+import { describe, it, expect, test } from "vitest";
+import Cart from "./Cart"
+
+describe("Cart Class", () => {
+    const testItem = {id: 1, title: "Test", price: 100}
+    it("adds new item to cartProducts", () => {
+        const cart = new Cart();
+
+        cart.addToCart(testItem, 5);
+
+        expect(cart.cartProducts).toContainEqual({item: testItem, quantity: 5})
+    })
+
+    it("addToCart increases quantity", () => {
+        const cart = new Cart();
+
+        cart.addToCart(testItem, 5);
+        cart.addToCart(testItem, 5);
+
+        expect(cart.cartProducts.length).toEqual(1);
+        expect(cart.cartProducts[0].quantity).toEqual(10);
+    })
+
+    it("removes item from a cart", () => {
+        const cart = new Cart();;
+
+        cart.addToCart(testItem, 5);
+        console.log(cart.cartProducts)
+        cart.removeFromCart(1);
+        console.log(cart.cartProducts)
+        expect(cart.cartProducts.length).toEqual(0);
+    })
+
+    it("changes quantity to an item", () => {
+        const cart = new Cart();
+
+        cart.addToCart(testItem, 5);
+        
+        cart.changeQuantity(1, 5);
+
+        expect(cart.cartProducts[0].quantity).toEqual(5);
+    })
+})
