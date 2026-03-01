f9c4e7cafe4e177ca27a4b5a086085dc56bb8d9a
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-03-01 12:17:26 +0100

    create router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
+import App from "./App"
+import Home, {loader as productLoader} from "./components/Home/Home"
+import Shop, {loader as shopLoader} from "./components/Shop/Shop"
+import Cart from "./components/Cart/Cart.jsx"
+import ErrorPage from "./components/ErrorPage/ErrorPage"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+        loader: productLoader
+      },
+      {
+        path: "shop",
+        element: <Shop />,
+        loader: shopLoader
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
