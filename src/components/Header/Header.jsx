539389e44018c4fdddbba2a2b4c553fc42614380
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-03-01 17:15:13 +0100

    create hide on scroll header animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,38 @@
 import Navigation from './Navigation/Navigation'
 import styles from './Header.module.css'
 import logo from '../../assets/logo/paulo_transparent.png'
+import { useEffect, useState } from 'react'
 
 function Header() {
+    const [show, setShow] = useState(true);
+    const [transparent, setTransparent] = useState(true)
+
+    useEffect(() => {
+        let lastScroll = window.scrollY;
+
+        function handleScroll() {
+            let currentScroll = window.scrollY;
+
+            if (currentScroll < 20) setTransparent(true);
+
+            if (currentScroll >= 20) setTransparent(false);
+
+            if (currentScroll > lastScroll) setShow(false);
+
+            if (currentScroll < lastScroll) setShow(true);
+
+            lastScroll = currentScroll
+        }
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll)
+
+    }, [])
+
     return (
-        <header>
-            <div className={styles.header}> 
+        <header className={`${styles.header} ${show ? "" : `${styles.hide}`} ${!transparent ? "" : `${styles.transparent}`}`}>
+            <div className={styles.headerDiv}> 
                 <img src={logo} alt="Logo" className={styles.logo} />
                 <Navigation />
             </div>
