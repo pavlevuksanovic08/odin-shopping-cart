d21b15a155de0bdb8de60b8b7a8d3e2469a60de4
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-03-02 23:49:11 +0100

    fix navigation 'a' to 'Link'

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -2,25 +2,26 @@ import styles from "./Navigation.module.css"
 import houseIcon from "../../../assets/icons/house.svg"
 import storeIcon from "../../../assets/icons/store.svg"
 import cartIcon from "../../../assets/icons/shopping-cart.svg"
+import { Link } from "react-router-dom"
 
 function Navigation() {
     return (
         <nav className={styles.navigation}>
             <ul className={styles.navigation}>
                 <li className={styles.iconContainer}>
-                    <a href="/" className={styles.navLink}>
+                    <Link href="/" className={styles.navLink}>
                         <img src={houseIcon} alt="Home" className={styles.navIcon} />
-                    </a>
+                    </Link>
                 </li>
                 <li className={styles.iconContainer}>
-                    <a href="/shop" className={styles.navLink}>
+                    <Link href="/shop" className={styles.navLink}>
                         <img src={storeIcon} alt="Shop" className={styles.navIcon} />
-                    </a>
+                    </Link>
                 </li>
                 <li className={styles.iconContainer}>
-                    <a href="/cart" className={styles.navLink}>
+                    <Link href="/cart" className={styles.navLink}>
                         <img src={cartIcon} alt="Cart" className={styles.navIcon} />
-                    </a>
+                    </Link>
                 </li>
             </ul>
         </nav>
