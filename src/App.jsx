7924b10fc462d77f88eeb2bdfa20a92b2de9b29b
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-03-03 19:40:12 +0100

    create cart object and pass it to outlet component as context

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header"
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 function App() {
+
+  const [cartItems, setCartItems] = useState([])
+  const cart = {
+    cartItems,
+    addToCart: (item, quantity) => {
+      setCartItems(prev => {
+        const existing = prev.find(i => i.item.id === item.id);
+        if (existing) {
+          return prev.map(i =>
+            i.item.id === item.id ? {...i, quantity: i.quantity + quantity} : i
+          );
+        }
+        return [...prev, {item, quantity}];
+      })
+    },
+    removeFromCart: (id) => {
+      setCartItems(prev => 
+        prev.filter(i => i.item.id !== id)
+      )
+    },
+    changeQuantity: (id, quantity) => {
+      setCartItems(prev => 
+        prev.map(i =>
+          i.item.id === id ? {...i, quantity: quantity} : i
+        )
+      )
+    }
+  }
 
   return (
     <>
       <Header />
-      <Outlet />
+      <Outlet context={cart}/>
       <Footer />
     </>
   )
