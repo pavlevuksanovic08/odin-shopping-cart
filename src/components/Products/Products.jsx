1a77c05de7371f669664dccbd7362a11206bec14
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-03-03 19:53:46 +0100

    import cart and pass it to children

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,14 +3,14 @@ import ProductCard from "./ProductCard/ProductCard"
 
 
 
-function Products({ products }) {
+function Products({ products, cart }) {
     return (
         <section data-testid="products-section" className={styles.container}>
             <h2 className={styles.heading}>BEST GIFTS</h2>
             <div className={styles.centerDiv}>
                 <div data-testid="products" className={styles.products}>
                     {products.map(product => (
-                        <ProductCard key={product.id} product={product}/>
+                        <ProductCard key={product.id} product={product} cart={cart}/>
                     ))}
                 </div>
             </div>
--- src/components/Products/Products.test.jsx
+++ src/components/Products/Products.test.jsx
@@ -3,8 +3,18 @@ import { screen, render } from "@testing-library/react";
 import Products from "./Products";
 
 describe("Products component", () => {
+
+  const testProducts = [
+    {id: 1, title: "Some title", price: 20},
+    {id: 2, title: "Some title", price: 20}
+  ]
+
+  const testCart = [
+    {item: {id: 1, title: "Some title", price: 20}, quantity: 2}
+  ]
+
   it("renders the Products component correctly", () => {
-    render(<Products />);
+    render(<Products products={testProducts} cart={testCart}/>);
     const productsSection = screen.getByTestId("products-section");
     const heading = screen.getByRole("heading", {name: "BEST GIFTS"});
     const products = screen.getByTestId("products");
