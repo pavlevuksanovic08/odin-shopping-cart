a7d57e42156769d705ececdc2424ea6cbe5581c5
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-03-04 00:02:45 +0100

    fix header rendering

--- src/components/Header/Header.test.jsx
+++ src/components/Header/Header.test.jsx
@@ -1,15 +1,24 @@
 import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import Header from './Header'
+import { MemoryRouter } from 'react-router-dom'
 
 describe('Header component', () => {
   it('renders the logo', () => {
-    render(<Header />)
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    )
     const logo = screen.getByAltText(/Logo/i)
     expect(logo).toBeInTheDocument()
   })
   it('renders the navigation', () => {
-    render(<Header />)
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    )
     const navigation = screen.getByRole('navigation')
     expect(navigation).toBeInTheDocument()
   })
