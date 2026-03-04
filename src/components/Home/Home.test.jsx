ce74b6d1d0e5e6f104a8d53d1dcbc40982eecedc
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-03-04 09:18:07 +0100

    mock useOutletContext and useLoadedData

--- src/components/Home/Home.test.jsx
+++ src/components/Home/Home.test.jsx
@@ -2,6 +2,21 @@ import {describe, it, expect} from 'vitest';
 import {render, screen} from '@testing-library/react';
 import Home from './Home';
 
+vi.mock("react-router-dom", async () => {
+  const actual = await vi.importActual("react-router-dom")
+  return {
+    ...actual,
+    useOutletContext: () => [
+      {item: {id: 1, title: "Title", price: 20}, quantity: 2}
+    ],
+    useLoaderData: () => [
+      {id: 1, title: "Title", price: 20}
+    ]
+  }
+})
+
+import { useLoaderData, useOutletContext } from 'react-router-dom';
+
 describe('Home component', () => {
   it('renders the Home component correctly', () => {
     render(<Home />);
