33389bc087cc5cb9b56e66795c95f65e1e607ff0
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-03-07 18:37:00 +0100

    scroll to top on every page change

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header"
 import { Outlet } from "react-router-dom";
 import { useState } from "react";
+import ScrollToTop from "./ScrollToTop";
 function App() {
 
   const [cartItems, setCartItems] = useState([])
@@ -36,6 +37,7 @@ function App() {
 
   return (
     <>
+      <ScrollToTop />
       <Header cartLength={cartItems.length}/>
       <Outlet context={cart}/>
       <Footer />
