3d765634469466369654c95a4ddf31bbcfc39f0c
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-03-07 13:28:30 +0100

    add dialog and show state

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,19 +1,25 @@
 import styles from "./Products.module.css"
 import ProductCard from "./ProductCard/ProductCard"
+import { useState } from "react";
+import Dialog from "../Dialog/Dialog";
 
 
 
 function Products({ products, cart }) {
+
+    const [show, setShow] = useState(false)
+
     return (
         <section data-testid="products-section" className={styles.container}>
             <h2 className={styles.heading}>BEST GIFTS</h2>
             <div className={styles.centerDiv}>
                 <div data-testid="products" className={styles.products}>
                     {products.map(product => (
-                        <ProductCard key={product.id} product={product} cart={cart}/>
+                        <ProductCard key={product.id} product={product} cart={cart} setShow={setShow} />
                     ))}
                 </div>
             </div>
+            <Dialog cart={cart} show={show} setShow={setShow} />
         </section>
     )
 }
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,6 +1,8 @@
 import ProductCard from "../Products/ProductCard/ProductCard"
 import { useLoaderData, useOutletContext } from "react-router-dom";
 import styles from "./Shop.module.css"
+import Dialog from "../Dialog/Dialog";
+import { useState } from "react";
 
 export async function loader() {
     const response = await fetch("https://fakestoreapi.com/products");
@@ -14,6 +16,8 @@ function Shop() {
 
     const cart = useOutletContext();
 
+    const [show, setShow] = useState(false);
+
     return (
         <main>
             <div className={styles.shop}>
@@ -21,12 +25,13 @@ function Shop() {
                 <div className={styles.productsContainer}>
                     <div className={styles.products}>
                         {products.map(product => (
-                            <ProductCard product={product} key={product.id} cart={cart}/>
+                            <ProductCard product={product} key={product.id} cart={cart} setShow={setShow} />
                         ))}
                     </div>
                 </div>
                 
             </div>
+            <Dialog cart={cart} show={show} setShow={setShow}/>
         </main>
     )
 }
